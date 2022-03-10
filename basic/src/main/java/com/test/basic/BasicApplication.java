5b2bf239124c8440ddbc287ce04b73ed239adbe3
JAEEOK <anjaeoek@nate.com>
2022-03-10 14:32:31 +0900

    2222

--- basic/src/main/java/com/test/basic/BasicApplication.java
+++ basic/src/main/java/com/test/basic/BasicApplication.java
@@ -2,10 +2,16 @@ package com.test.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BasicApplication {
 
+	@GetMapping(value="/test")
+	public String aaa(){
+		return "asd";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BasicApplication.class, args);
 	}
--- basic/src/test/java/com/test/basic/BasicApplicationTests.java
+++ basic/src/test/java/com/test/basic/BasicApplicationTests.java
@@ -2,18 +2,11 @@ package com.test.basic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootTest
 class BasicApplicationTests {
 
 
-	@GetMapping(value="/test")
-	public String aaa(){
-		return "asd";
-	}
 
 	@Test
 	void contextLoads() {
